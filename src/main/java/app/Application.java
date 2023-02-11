1d2d7a2e8d4da216bd0877e6f607c40c026da5f4
GlazovYuriu <Pavelandreev2006@gmail.com>
2023-02-11 10:45:54 +0300

    text centering works

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -43,20 +43,12 @@ public class Application implements Consumer<Event> {
     public Application() {
         // создаём окно
         window = App.makeWindow();
+
+        // создаём первый заголовок
+        label = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
+                "Привет, мир!", true, true);
         // задаём обработчиком событий текущий объект
         window.setEventListener(this);
-        // задаём заголовок
-        window.setTitle("Java 2D");
-        // задаём размер окна
-        window.setWindowSize(900, 900);
-        // задаём его положение
-        window.setWindowPosition(100, 100);
-
-
-
-        // задаём иконку
-        label = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, "Привет, мир!");
-
 
 
 
@@ -121,11 +113,11 @@ public class Application implements Consumer<Event> {
         canvas.save();
         // очищаем канвас
         canvas.clear(APP_BACKGROUND_COLOR);
-        // рисуем заголовок
-        label.paint(canvas, windowCS);
-        // восстанавливаем состояние канваса
-        canvas.restore();
         // рисуем заголовок в точке [100,100] с шириной и выостой 200
         label.paint(canvas, new CoordinateSystem2i(100, 100, 200, 200));
+
+        // восстанавливаем состояние канваса
+        canvas.restore();
+
     }
 }
